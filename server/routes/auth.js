f7dd644b449bcd952a6695be8043d951a0f82ab8
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-07 10:43:03 +1200

    generate token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,27 +1,33 @@
 const express = require('express')
 const router = express.Router()
-const {createUser} = require('../db/users')
+const token = require('../auth/token')
+const { createUser } = require('../db/users')
 
-router.post('/register', register)
+router.post('/register', register, token.issue)
 
-function register (req, res) {
-    const {username, password} = req.body
-    createUser({username, password})
-      .then(() => res.status(201).json({ok: true}))
-      .catch(({message}) => {
-        // This is vulnerable to changing databases. SQLite happens to use
-        // this message, but Postgres doesn't.  
-        if (message.includes('UNIQUE constraint failed: users.username')) {
-          return res.status(400).json({
-            ok: false,
-            message: 'Username already exists.'
-          })
-        }
-        res.status(500).json({
-          ok: false,
-          message: "Something bad happened. We don't know why."
+function register(req, res, next) {
+    const { username, password } = req.body
+    createUser({ username, password })
+        .then(([id]) => {
+            // Be sure to grab the id out of the array Knex returns it in!
+            // You can use array destructuring (as above) if you like.
+           res.locals.userId = id
+           next()
         })
-      })
-  }
+        .catch(({ message }) => {
+            // This is vulnerable to changing databases. SQLite happens to use
+            // this message, but Postgres doesn't.  
+            if (message.includes('UNIQUE constraint failed: users.username')) {
+                return res.status(400).json({
+                    ok: false,
+                    message: 'Username already exists.'
+                })
+            }
+            res.status(500).json({
+                ok: false,
+                message: "Something bad happened. We don't know why."
+            })
+        })
+}
 
 module.exports = router
