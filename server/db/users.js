3f513db38b7efa13176ec350d870f950d8c1b007
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-07 10:06:28 +1200

    add hash password

--- server/db/users.js
+++ server/db/users.js
@@ -1,9 +1,11 @@
 const connection = require('./connection')
+const {generateHash} = require('../auth/hash')
 
 module.exports = {
   createUser
 }
 
 function createUser ({username, password}, db = connection) {
-  return db('users').insert({username, hash: password})
-}
+    return generateHash(password)
+      .then(hash => db('users').insert({username, hash}))
+  }
