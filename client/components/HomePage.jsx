b1444a0b11a62044a9c6cd294f3f6ff350c442e0
tcodling <thomas.codling@outlook.com>
2020-05-08 10:12:49 +1200

    add style to intro section

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <h1>HomePage</h1>
+    <div id='intro'>
+        <h1 id='heading'>PlantHeads</h1>
+        <h2>For people who have a plant for a head</h2>
+    </div>
   )
 }
 
