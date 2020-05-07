9f238bb16b03f81792bdc832633c2c6d74f5c938
tcodling <thomas.codling@outlook.com>
2020-05-07 16:11:16 +1200

    add temperature styles

--- client/components/Temperature.jsx
+++ client/components/Temperature.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
-function PhMeter(props) {
+function Temperature(props) {
+    let style = {}
+
+    if (props.temp <= 0) {
+      style.backgroundColor = 'rgb(184, 184, 255)';
+    } else {
+      style.backgroundColor = 'rgb(241, 97, 40)';
+    }
+
     return (
-      <div id='temperature'>
+      <div id='temperature' style={style}>
           <p>Min Temp</p>
-          <p>{props.temp.toFixed(1)}</p>
+          <p>{props.temp.toFixed(1)}°</p>
       </div>
     )
   }
   
-  export default PhMeter
+  export default Temperature
