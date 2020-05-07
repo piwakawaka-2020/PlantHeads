e84b2c0266ff99f82c789ab1a2f87a0886b22a3e
tcodling <thomas.codling@outlook.com>
2020-05-08 10:17:55 +1200

    add search bar that doesnt work

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -5,6 +5,7 @@ function HomePage() {
     <div id='intro'>
         <h1 id='heading'>PlantHeads</h1>
         <h2>For people who have a plant for a head</h2>
+        <input placeholder='Search for a plant!' id='search' />
     </div>
   )
 }
