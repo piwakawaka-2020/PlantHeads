ef826ff880647744f009e9e6d71e50b3d97dd45b
tcodling <thomas.codling@outlook.com>
2020-05-07 16:13:34 +1200

    add drought tolerance style

--- client/components/DroughtTolerance.jsx
+++ client/components/DroughtTolerance.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function DroughtTolerance(props) {
+    let style = {}
+
+    if (props.drought === 'High') {
+      style.backgroundColor = 'rgb(233, 155, 53)'
+    } else {
+      style.backgroundColor = 'rgb(235, 190, 132)'
+    }
+
     return (
-      <div id='drought'>
+      <div id='drought' style={style}>
           <p>Drought</p>
           <p>Tolerance</p>
           <p>{props.drought}</p>
