e97f05069918f705925114424120d8db136a1f5b
tcodling <thomas.codling@outlook.com>
2020-05-07 15:56:27 +1200

    add toxicity style changes

--- client/components/Toxicity.jsx
+++ client/components/Toxicity.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function Toxicity(props) {
+    let style = {}
+
+    if (props.toxicity === 'Severe') {
+      style.backgroundColor = 'green'
+      style.color = 'white'
+    } else {
+      style.backgroundColor = 'rgb(125, 209, 125)'
+    }
+
     return (
-      <div id='toxicity'>
+      <div id='toxicity' style={style}>
           <p>Toxicity</p>
-          <p>{props.toxicity}</p>
+          <p>{props.toxicity ? props.toxicity : 'No data'}</p>
       </div>
     )
   }
