22375d11e2341c450c0d44609e5fefb40a28c98d
tcodling <thomas.codling@outlook.com>
2020-05-07 14:48:11 +1200

    add height styles

--- client/components/Height.jsx
+++ client/components/Height.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Height(props) {
     return (
+        <>
         <div id='height'>
-            
+            <p>Max Height: {props.height}cm</p>
+            <div id='pot'></div>
         </div>
+        </>
     )
 }
 
