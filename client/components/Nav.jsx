366f04a2ccef3e44916ead57eef55e08bf17df79
tcodling <thomas.codling@outlook.com>
2020-05-08 11:14:54 +1200

    style

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 function Nav() {
   return (
     <div id='navBar'>
-        <h2 id='navHeading'>PlantHeads</h2>
+        <h2 id='navHeading'><img id='icon' src='images/plantIcon.svg' />PlantHeads</h2>
         <ul>
             <li>Login</li>
             <li>Register</li>
             <li>Saved Plants</li>
+            <li>Buy/Sell Plants</li>
             <li>Home</li>
         </ul>
     </div>
--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Results(props) {
   return (
-    <div>
+    <div id='resultSection'>
         <h3>Search Results</h3>
         <div id='results'>
-            {props.results.map(result => <h2 className='result'>{result.common_name}</h2>)}
+            {props.results.map(result => <h2 className='result'>{result.scientific_name} {result.common_name ? `(${result.common_name})`: ''}</h2>)}
         </div>
     </div>
   )
