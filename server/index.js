8228eac6d08a1cc84415628d0a6f71a89862ed48
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-08 14:42:36 +1200

    register feature

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000
 const envConfig = require('dotenv').config()
 if(envConfig.error) throw envConfig.error
 
+const server = require('./server')
+
 server.listen(port, function () {
   console.log('Listening on port', port)
 })
