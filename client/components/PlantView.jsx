c2caf3b8875c78886b75606a9aa35782c1cd63e2
tcodling <thomas.codling@outlook.com>
2020-05-09 10:27:24 +1200

    style

--- client/components/PlantView.jsx
+++ client/components/PlantView.jsx
@@ -55,7 +55,7 @@ class PlantView extends React.Component {
                 <PlantViewListings listings={listings} />
             </div>
         </div>
-        ) : 'Loading'
+        ) : <img className='oddishLoading' src='images/oddish.gif' />
     }
 }
 
