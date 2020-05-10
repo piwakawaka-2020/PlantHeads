17384ab548b8147ebd064f98081a811b401f4e15
Pat Gonzales <patvalentinegonzales@gmail.com>
2020-05-11 01:44:33 +1200

    working createListing component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,7 +15,18 @@ const App = () => {
     {/* <Register />
     <Login /> */}
     {/* <HomePage /> */}
-    <CreateListing />
+    <CreateListing 
+      user='bossManThomas' 
+      usersId={3}
+      plantsId={132811}
+      contact='0221237654' 
+      email='thomas@bossMan.co.nz'
+      scientificName='Pothos Aurea'
+      commonName='Devils Ivy'/>
+
+      {/* details under createListing hardcoded to have results*/}
+      {/* need to request this as props for the actual caller of CreateListing Component*/}
+    
     </>
   )
 }
