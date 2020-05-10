403700107603796258fcf00cc6192cd3c9193bc4
tcodling <thomas.codling@outlook.com>
2020-05-11 10:08:50 +1200

    router fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,24 +13,6 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 const App = () => {
   return (
     <>
-
-<!--     <Nav />
-    {/* <Register />
-    <Login /> */}
-    {/* <HomePage /> */}
-    <CreateListing 
-      user='bossManThomas' 
-      usersId={3}
-      plantsId={132811}
-      contact='0221237654' 
-      email='thomas@bossMan.co.nz'
-      scientificName='Pothos Aurea'
-      commonName='Devils Ivy'/> -->
-
-      {/* details under createListing hardcoded to have results*/}
-      {/* need to request this as props for the actual caller of CreateListing Component*/}
-    
-
     <Router>
       <Route path='/' component={Nav} />
       <Route exact path="/" component={HomePage} />
@@ -38,7 +20,6 @@ const App = () => {
       <Route exact path="/register" component={Register} />
       <Route exact path="/plant/:plantId" component={PlantView} />
     </Router>
-
     </>
   )
 }
