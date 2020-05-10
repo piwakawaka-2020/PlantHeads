95d820c02c52ab1165420b60240596b662bfcbb1
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-11 09:39:02 +1200

    fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,30 +14,30 @@ const App = () => {
   return (
     <>
 
-<!--     <Nav />
-    {/* <Register />
+      <Nav />
+      {/* <Register />
     <Login /> */}
-    {/* <HomePage /> */}
-    <CreateListing 
-      user='bossManThomas' 
-      usersId={3}
-      plantsId={132811}
-      contact='0221237654' 
-      email='thomas@bossMan.co.nz'
-      scientificName='Pothos Aurea'
-      commonName='Devils Ivy'/> -->
+      {/* <HomePage /> */}
+      <CreateListing
+        user='bossManThomas'
+        usersId={3}
+        plantsId={132811}
+        contact='0221237654'
+        email='thomas@bossMan.co.nz'
+        scientificName='Pothos Aurea'
+        commonName='Devils Ivy' /> -->
 
       {/* details under createListing hardcoded to have results*/}
       {/* need to request this as props for the actual caller of CreateListing Component*/}
-    
 
-    <Router>
-      <Route path='/' component={Nav} />
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/plant/:plantId" component={PlantView} />
-    </Router>
+
+      <Router>
+        <Route path='/' component={Nav} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/plant/:plantId" component={PlantView} />
+      </Router>
 
     </>
   )
