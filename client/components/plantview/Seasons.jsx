159650307ffa98ac3428f1ccd7e773380b762b19
tcodling <thomas.codling@outlook.com>
2020-05-12 11:34:36 +1200

    fix button style

--- client/components/plantview/Seasons.jsx
+++ client/components/plantview/Seasons.jsx
@@ -7,10 +7,10 @@ function Seasons(props) {
       <div>
           <h3>Seasonal info</h3>
           <div id='seasons'>
-            <div id='summer' className='season'>Summer<br/>{seasons.indexOf('Summer') > -1 ? 'Growth': ''}</div>
-            <div id='autumn' className='season'>Autumn<br/>{seasons.indexOf('Autumn') > -1 ? 'Growth': ''}</div>
-            <div id='winter' className='season'>Winter<br/>{seasons.indexOf('Winter') > -1 ? 'Growth': ''}</div>
-            <div id='spring' className='season'>Spring<br/>{seasons.indexOf('Spring') > -1 ? 'Growth': ''}</div>
+            <div id='summer' className='season'>Summer<br/>{seasons.indexOf('Summer') > -1 ? <span className='growthTime'>Growth</span>: ''}</div>
+            <div id='autumn' className='season'>Autumn<br/>{seasons.indexOf('Autumn') > -1 ? <span className='growthTime'>Growth</span>: ''}</div>
+            <div id='winter' className='season'>Winter<br/>{seasons.indexOf('Winter') > -1 ? <span className='growthTime'>Growth</span>: ''}</div>
+            <div id='spring' className='season'>Spring<br/>{seasons.indexOf('Spring') > -1 ? <span className='growthTime'>Growth</span>: ''}</div>
           </div>
       </div>
     )
