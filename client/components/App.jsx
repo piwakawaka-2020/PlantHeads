c49514ed78cd96307a1b5bc832c7a0bd935d6aca
Pat Gonzales <patvalentinegonzales@gmail.com>
2020-05-12 02:38:45 +1200

    edit route for createListing and add hardcode for users

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,6 +13,14 @@ import ListingView from './ListingView'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
+
+  //This should be fetched from the login session, currently having an error with login/register API
+  const tempUser = {
+    username:'bossManThomas',
+    usersId:3,
+    email:'thomas@bossMan.co.nz'
+  }
+
   return (
     <>
       <Router>
@@ -22,7 +30,10 @@ const App = () => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/plant/:plantId" component={PlantView} />
         <Route exact path="/savedPlants" component={SavedPlants} />
-        <Route exact path="/createListing" component={CreateListing} />
+        {/* this route for CreateListing is accessed through PlantView */}
+        <Route exact path="/createListing/:plantsId" render={(props) => <CreateListing {...props} {...tempUser} />} />
+        {/* this CreateListing route accessed through the ListingView */}
+        <Route exact path="/createListing" render={(props) => <CreateListing {...props} {...tempUser} />} />
         <Route exact path="/listings" component={ListingView} />
       </Router>
       </>
