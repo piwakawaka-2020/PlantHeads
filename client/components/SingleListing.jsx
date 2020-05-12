5e74388703272f42d2361a387e0c366266075a47
Josh Wood <JHW300@gmail.com>
2020-05-13 11:37:12 +1200

    single listing css

--- client/components/SingleListing.jsx
+++ client/components/SingleListing.jsx
@@ -25,8 +25,8 @@ class SingleListing extends React.Component {
         
         return (
             <>
-            <div className='single-list-border'>
-                <div className='single-list-container'>
+            <div className='single-list-container'>
+                    <div className='single-list-border'>
                         <div className='header-container'>
                             <h2 className='heading-single-listing'>House Plants For Sale</h2>
                         </div>
@@ -39,16 +39,18 @@ class SingleListing extends React.Component {
                         <div className='single-plant-listing'>
                                 <p className='listingInfo'>Botanical Name: {this.state.singlelisting.scientific_name}</p>
                                 <p className='listingInfo'>Common Name: {this.state.singlelisting.common_name}</p>
-                                <p className='listingInfo'>${this.state.singlelisting.cost}</p> 
+                                <p className='listingInfo'>Plant Type: {this.state.singlelisting.type}</p> 
                                 <p className='listingInfo'>Care Tips: {this.state.singlelisting.care_tips}</p> 
-                                <p className='listingInfo'>Plant Size: {this.state.singlelisting.type}</p> 
+                                <p className='listingInfo'>Cost: ${this.state.singlelisting.cost}</p> 
+                            <div className='linkToContactSeller'>
+                                <h2 className='contactSellerStyle'>Buy This Plant ~ Contact The Seller</h2>
+                                <Link to='/emailForm'><button id='save'>Contact Seller</button></Link> 
+                            </div>
                         </div>
-                    <div className='linkToContactSeller'>
-                        <h2 className='contactSellerStyle'>Buy This Plant ~ Contact Your Seller</h2>
-                        <Link to='/emailForm'><button id='save'>Contact Seller</button></Link> 
-                    </div>  
                     </div>
+                    
                 </div>
+                
             </div>
             </>
             
