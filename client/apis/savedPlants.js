532ee7dd3e8ce2b524591fc5f38332bd19d10cd5
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-13 10:30:38 +1200

    properly decodes

--- client/apis/savedPlants.js
+++ client/apis/savedPlants.js
@@ -1,15 +1,35 @@
 import request from 'superagent'
+import { isAuthenticated, removeUser } from '../utils/auth'
+import { get } from '../utils/localstorage'
+
 
 export function savePlant(plant) {
+    const token = get('token')
+    const headers = { Accept: 'application/json' }
+
+    if (isAuthenticated()) {
+        headers['Authorization'] = `Bearer ${token}`
+    }
+
     return request
         .post('/api/savedPlants/')
+        .set(headers)
         .send(plant)
         .then(res => res.body)
 }
 
 export function getAllSavedPlants(userId) {
-    return request.get(`/api/savedPlants/${userId}`)
+    const token = get('token')
+    const headers = { Accept: 'application/json' }
+
+    if (isAuthenticated()) {
+        headers['Authorization'] = `Bearer ${token}`
+    }
+
+    return request
+        .get(`/api/savedPlants/${userId}`)
+        .set(headers)
         .then(res => res.body)
-} 
+}
 
 //someone wrote this I already had one in plants called getAllSavedPlants > Alice
