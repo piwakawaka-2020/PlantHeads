ac8c90eb714646939bc6f22013cac3e1dc7993c0
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-13 10:34:43 +1200

    tidy up

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { getUserTokenInfo } from '../utils/auth'
 import { logoutUser } from '../actions/logout'
 
 class Nav extends React.Component {
+  
   render() {
-    const user = getUserTokenInfo()
     return (
       <div id='navBar'>
         <h2 id='navHeading'><img id='icon' src='images/plantIcon.svg' />PlantHeads</h2>
         <ul>
-          {/* {user.length > 0 ? <></> : <Link to='/login'><li>Login</li></Link>}
-            {user.length > 0 ? <></> : <Link to='/register'><li>Register</li></Link>} */}
+
             {this.props.auth.isAuthenticated ? 
             <>
             <Link to='/'><li onClick={() => this.props.logout()}>Logout</li></Link>
