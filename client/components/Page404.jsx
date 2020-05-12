568064cbbc0b76a9b629e4b6d498217a32cfe262
Kevin Guan <60491871+kguan880@users.noreply.github.com>
2020-05-12 15:56:31 +1200

    bellsprout image

--- client/components/Page404.jsx
+++ client/components/Page404.jsx
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Page404 = () => {
     return (
-        <>
-            Oops, did you get ahead of yourself young sprout?
-            Let's get you signed in first!
+        <div id='pageNotFound'>
+            <h1>Oops, did you get ahead of yourself young sprout?</h1>
+            <h2> Let's get you signed in first!</h2>
+            <br/>
             <Link to='/login'>
-                <button>Login</button>
+                <button className='greenHover'>Login</button>
             </Link>
-        </>
+            <div>
+                <img className='bellsprout' src="https://pbs.twimg.com/media/EV1AeH_XQAAeMB2.jpg" />
+            </div>
+        </div>
     )
 }
 
