83dd92489692712f02ed4ccb56137cb7f767811d
Alice Jowsey <12345alcatraz@gmail.com>
2020-05-12 22:49:21 +1200

    css on single listing

--- client/components/SingleListing.jsx
+++ client/components/SingleListing.jsx
@@ -24,21 +24,28 @@ class SingleListing extends React.Component {
         
         return (
             <>
-            <div className='listingViewHeader'>
-                <h2>House Plants For Sale</h2>
-            </div>
-             <div className='listingViewContainer'>
-                <div className='singleListingView'>
-                    <img className='listingPic' src="/images/placeholder.jpg" alt="photo of tree"/>
-                    <p className='listingInfo'>Botanical Name: {this.state.singlelisting.scientific_name}</p>
-                    <p className='listingInfo'>Common Name: {this.state.singlelisting.notes}</p>
-                    <p className='listingInfo'>${this.state.singlelisting.cost}</p> 
-                    <p className='listingInfo'>${this.state.singlelisting.user_id}</p> 
-                </div> 
-            </div> 
-            <div className='linkToCreateListing'>
-                <h2 className='createListingHeader'>To Give A Plant To Another Loving Family</h2>
-                <Link to='/createListing'><button id='save'>Create Listing</button></Link>
+            <div className='single-list-container'>
+                <div className='single-list-border'>
+                    <div className='header-container'>
+                        <h2 className='heading-single-listing'>House Plants For Sale</h2>
+                    </div>
+                    <div className='single-list-box'>
+                            {/* <img className='img-single-list' src="/images/placeholder.jpg" alt="photo of tree"/>   */}     
+                    </div> 
+                    <div className='single-plant-listing'>
+                            <p className='listingInfo'>Botanical Name: {this.state.singlelisting.scientific_name}</p>
+                            <p className='listingInfo'>Notes: {this.state.singlelisting.notes}</p>
+                            <p className='listingInfo'>Common Name: {this.state.singlelisting.common_name}</p>
+                            <p className='listingInfo'>${this.state.singlelisting.cost}</p> 
+                            <p className='listingInfo'>Care Tips: {this.state.singlelisting.care_tips}</p> 
+                            <p className='listingInfo'>Plant Size: {this.state.singlelisting.type}</p> 
+                            <p className='listingInfo'>{this.state.singlelisting.users_id}</p> 
+                    </div>
+                    {/* <div className='linkToCreateListing'>
+                        <h2 className='createListingHeader'>Buy This Plant ~ Contact Your Seller</h2>
+                        <Link to='/createListing'><button id='save'>Create Listing</button></Link> 
+                    </div>  */}
+                </div>
             </div>
             </>
             
